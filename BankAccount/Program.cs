e56c816d278a7b5fff96ba0aec2bc81c3466488d
davischristianson <dav.christian10@gmail.com>
2023-08-29 13:49:07 -0700

    Added some test code

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BankAccount;
+
+Account myAccount = new Account("John Smith");
+myAccount.Deposit(500);
